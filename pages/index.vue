04b0d82e6754b86aa035954b1caab9fa42ea069c
oyham <voyhamburusteam@gmail.com>
2024-09-02 16:00:44 -0300

    add files

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="border-b-2 flex justify-end px-6 py-6">
+        <NuxtLink to="/" class="mr-2">Home</NuxtLink>
+        <NuxtLink to="/posts">Post</NuxtLink>
+    </header>
+    <h1 class="text-3xl">Home</h1>
+</template>
