a9c552acc2dc519b0655543689dc838ea530d44b
oyham <voyhamburusteam@gmail.com>
2024-09-03 15:41:36 -0300

    add post route

---
+++ pages/posts/[id].vue
@@ -0,0 +1,13 @@
+<script setup>
+// Composable de useRoute dónde se guardan todas las rutas 
+const route = useRoute()
+
+const {data: post} = await useFetch(`https://jsonplaceholder.typicode.com/posts/${route.params.id}`)
+</script>
+
+<template>
+    <!-- $route (palabra reservada) de nuxt para uso en template -->
+    <!-- <h1>Detail page id:{{ $route.params.id }}</h1> -->
+    <h1 class="text-3xl">{{post.title}}</h1>
+    <p class="m-4">{{post.body}}</p>
+</template>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -14,7 +14,6 @@ posts.value = await res.json() */
 // Destructuring de data para llamarse post, haciendo uso de useFetch (no es necesario importarlo)
 const {data: posts} = await useFetch('https://jsonplaceholder.typicode.com/posts')
 
-
 </script>
 
 <template>
@@ -23,11 +22,11 @@ const {data: posts} = await useFetch('https://jsonplaceholder.typicode.com/posts
         <article v-for="{ title, id } in posts" :key="id" class="w-64 hover:animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s]">
             <div class="rounded-[10px] bg-white p-4 !pt-20 sm:p-6">
                 <time datetime="2022-10-10" class="block text-xs text-gray-500"> 10th Oct 2022 </time>
-                <a href="#">
+                <NuxtLink :to="`/posts/${id}`">
                 <h3 class="mt-0.5 text-lg font-medium text-gray-900">
                     {{title}}
                 </h3>
-                </a>
+                </NuxtLink>
             </div>
         </article>
     </section>
